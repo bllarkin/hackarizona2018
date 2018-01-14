2ddb72f12ab9727d1aaab55e0f53918e1fe4d969
Brandon Larkin <bllarkin@gmail.com>
2018-01-13 22:22:39 -0700

    Uncomment missed GCP settings

--- server.js
+++ server.js
@@ -289,8 +289,8 @@ const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-//   console.log('Example app listening at http://${host}:${port}');
-// });
+   console.log('Example app listening at http://${host}:${port}');
+ });
 
 
 // App functions
